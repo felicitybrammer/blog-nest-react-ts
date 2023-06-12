1e2221080924afc8f957c611e937f15b7c496ffc
Felicity Brammer <felicitybrammer@gmail.com>
2023-06-12 13:59:49 -0700

    listed routes and components in App.tsx, no render bug

--- blog-frontend/src/App.tsx
+++ blog-frontend/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import Home from './components/Home';
+import Post from './components/post/Post';
+import Edit from './components/post/Edit';
+import Create from './components/post/Create';
 
-function App() {
+function App(): React.ReactElement {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className={'container'}>
+        <Router>
+          <Routes>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/post/:postId"} element={<Post />} />
+            <Route path={"/edit/:postId"} element={<Edit />} />
+            <Route path={"/create"} element={<Create />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
